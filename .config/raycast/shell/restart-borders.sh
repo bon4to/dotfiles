67a8f8394fbe7587bd98dd3da8aef6d2ee53c0f7
Lucas G. Bonato <lucasbon4to@gmail.com>
2025-08-22 15:22:37 -0300

    add raycast shell scripts

---
+++ .config/raycast/shell/restart-borders.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Restart Borders
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon ↪️
+
+# Documentation:
+# @raycast.author bon4to
+# @raycast.authorURL https://raycast.com/bon4to
+
+brew services restart borders
+
---
+++ .config/raycast/shell/stop-borders.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Stop Borders
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon ⏹️
+
+# Documentation:
+# @raycast.author bon4to
+# @raycast.authorURL https://raycast.com/bon4to
+
+brew services stop borders
